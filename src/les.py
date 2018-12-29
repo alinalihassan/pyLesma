8b60c21633b03bc25fe3a0fc2584d16c78fdccbb
Alin Ali Hassan <hassanalinali@gmail.com>
2018-12-29 13:12:54 +0100

    Bumped version to 0.2.0

--- src/les.py
+++ src/les.py
@@ -68,7 +68,7 @@ def _compile(arg_list):
 
 
 if __name__ == "__main__":
-    args = docopt(__doc__, version='0.1.2')
+    args = docopt(__doc__, version='0.2.0')
 
     if args['compile']:
         _compile(args)
