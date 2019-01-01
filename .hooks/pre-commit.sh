2641c62556dcd4dd4388167fbef66c73738b2dbb
Alin Ali Hassan <hassanalinali@gmail.com>
2019-01-01 16:18:31 +0100

    Added git hooks with pytest and master protection

---
+++ .hooks/pre-commit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Pytest
+echo "Running pre-commit hook"
+pytest -vv --cache-clear
+
+if [ $? -ne 0 ]; then
+ echo "Tests must pass before committing"
+ exit 1
+fi
+####
+
+# Prevent master commit
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$branch" = "master" ]; then
+  echo "You can't commit directly to master branch"
+  exit 1
+fi
+####
