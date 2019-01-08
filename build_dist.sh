ea74957dc428c6712612bf77c06962e2f7f5925d
Alin Ali Hassan <hassanalinali@gmail.com>
2019-01-08 19:38:51 +0100

    Reverting dist to pyinstaller until nuitka gets fixed

--- build_dist.sh
+++ build_dist.sh
@@ -1,1 +1,1 @@
-python -m nuitka src/les.py --include-package=lesma --standalone --nofollow-imports --remove-output --python-flag=no_site --plugin-disable=pylint-warnings
+pyinstaller src/les.py -D -n lesma
